ae17948e42e7c2dd261b9583e375f62da8b4bc0d
Thomas Lisankie <thomas.lisankie@gmail.com>
2018-07-08 14:11:18 -0400

    Fixed XML to JSON conversion so resulting format would be more useful later

--- xml-to-json.py
+++ xml-to-json.py
@@ -4,35 +4,45 @@ from bs4 import BeautifulSoup
 import os
 import json
 from os.path import join, isfile, dirname
-from unidecode import unidecode
+
+# Needs to pour all posts into a single file where each post has an age and gender attached to it.
 
 xmlDataDirPath = join(dirname(__file__), 'data/blogs/xml-data')
 jsonDataDirPath = join(dirname(__file__), 'data/blogs/json-data')
 
-def xml_node_to_json(node, gender, age):
-    # Needs to get all dates and posts from each blog
-
-    return {
-        'posts': [elem.text.replace("\n", "").replace("\t", "").strip() for elem in node.findAll('post')],
-        'gender': gender,
-        'age': age
-    }
-
+''' Returns a single data sample to be appended to the JSON file'''
+def get_single_sample(post, gender, age):
+    return {'post': post, 'gender': gender, 'age': age}
 
 files = [f for f in os.listdir(xmlDataDirPath) if isfile(join(xmlDataDirPath, f))]
-
+counter = 0
+jsonName = "the-data.json"
 for xmlName in files:
     print("Starting on", xmlName)
-    jsonName = xmlName.replace('.xml', '.json')
-    fileNameElements = xmlName.split('.')
+    fileNameElements = xmlName.split('.') # So we can get the gender and age of the person
+
+    # Load what's already in the JSON file into memory
+    try:
+        with open(join(jsonDataDirPath, jsonName)) as r_json:
+            current_json_data = json.load(r_json)
+            if current_json_data is None:
+                current_json_data = []
+    except IOError:
+        print("Could not read file, starting from scratch")
+        current_json_data = []
+
     with open(join(xmlDataDirPath, xmlName), "rb") as rf:
         with open(join(jsonDataDirPath, jsonName), mode='w') as wf:
             content = BeautifulSoup(rf, features = "xml")
 
-            jsonDocs = []
+            new_data = []
             for entry in content.findAll('Blog'):
-                data = xml_node_to_json(entry, fileNameElements[1], fileNameElements[2])
-                jsonDocs.append(data)
-
-            json.dump(jsonDocs, wf, indent=4, sort_keys=False)
+                posts = [elem.text.replace("\n", "").replace("\t", "").strip() for elem in entry.findAll('post')]
+                for post in posts:
+                    new_data.append(get_single_sample(post, fileNameElements[1], fileNameElements[2]))
+            
+            for instance in new_data:
+                current_json_data.append(instance)
+            
+            json.dump(current_json_data, wf, indent=4, sort_keys=False)
             print("Done with", xmlName)
