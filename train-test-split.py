bfaed32a9375ad0b086287b3f090e140e936d45a
Thomas Lisankie <thomas.lisankie@gmail.com>
2018-07-11 13:28:51 -0400

    Wrote code to split the total data into train and test sets

---
+++ train-test-split.py
@@ -0,0 +1,38 @@
+import json
+from random import shuffle
+from math import ceil
+
+JSON_DIR = "data/blogs/json-data/"
+input_file_path = JSON_DIR + "the-data.json"
+train_file_path = JSON_DIR + "train.json"
+test_file_path = JSON_DIR + "test.json"
+train_split = 0.8
+
+print("Loading all data")
+json_data = json.load(input_file_path)
+print("Data loaded.")
+
+print("Shuffling the data")
+shuffle(json_data)
+print("Data shuffled.")
+
+train_sample_count = int(ceil(train_split * total_data_instances))
+test_sample_count = total_data_instances - train_sample_count
+
+print("Creating a training set of", train_sample_count, "instances.")
+train_list = json_data[ : train_sample_count]
+print("Training set created.")
+
+print("Creating a testing set of", train_sample_count, "instances.")
+test_list = json_data[train_sample_count : ]
+print("Testing set created.")
+
+with open(train_file_path, mode='w') as wf:
+    print("Saving training set")
+    json.dump(train_list, wf, indent=4, sort_keys=False)
+    print("Training set saved.")
+
+with open(test_file_path, mode='w') as wf:
+    print("Saving testing set")
+    json.dump(test_list, wf, indent=4, sort_keys=False)
+    print("Testing set saved.")
